04dafc90de1cc0931324e87a9b90116262cf4449
liu <2536733617@qq.com>
2021-09-10 18:15:27 +0800

    init

---
+++ parser/webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path')
+
+module.exports = {
+  entry: path.resolve(__dirname, './src/index'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    library: {
+      name: 'Parser',
+      type: 'var'
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+};
